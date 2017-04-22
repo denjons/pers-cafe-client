598568b5125ec7e3d8c4a23769568edd49dd5c31
denjons <dennis.j@live.se>
2017-04-22 13:59:20 +0200

    before pulling add product

--- gbg-cafe/src/app/core/urls.ts
+++ gbg-cafe/src/app/core/urls.ts
@@ -1,8 +1,8 @@
 
 export class Urls {
-    //static base = "http://158.234.153.47:8080/cafe-app/webapi";
-    //static base = "http://localhost:8081/cafe-app/webapi";
     static base = "http://localhost:8080/cafe-app/webapi";
+    //static base = "http://localhost:8081/cafe-app/webapi";
+   // static base = "http://158.234.155.43:8080/cafe-app/webapi";
     static login = Urls.base + "/user/login";
     static getShop = Urls.base + "/shop/get";
     static purchase = Urls.base + "/shop/purchase";
